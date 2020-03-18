b6197a9b602fa5be2690b8a4fb93be61f27baf08
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2020-03-18 13:19:44 +0530

    Add files via upload

--- shop/main/urls.py
+++ shop/main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from django.shortcuts import render
-from .views import (index, detail, HomeView, checkout, DetailProductView, add_to_cart,remove_from_cart)
+from .views import (index, detail, HomeView, checkout, DetailProductView, add_to_cart, remove_from_cart, OrderSummary)
 
 app_name = 'main'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('index/', index, name="index"),
     path('product/<slug>', DetailProductView.as_view(), name="product-detail"),
     path('add-to-cart/<slug>', add_to_cart, name="add-to-cart"),
+    path('order-summary/', OrderSummary.as_view(), name="order-summary"),
     path('remove-from-cart/<slug>', remove_from_cart, name="remove-from-cart"),
 ]
