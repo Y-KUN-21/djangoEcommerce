c784aeb415dcc1e26b81bbee8266620caaefc810
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2020-03-26 21:40:49 +0530

    Update urls.py

--- shop/main/urls.py
+++ shop/main/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path, include
-from django.shortcuts import render
-from .views import (index, detail, HomeView, checkout, DetailProductView, add_to_cart, remove_from_cart, OrderSummary,
-                    remove_single_item_from_cart)
+from .views import (index, detail, HomeView, CheckoutView, DetailProductView, add_to_cart, remove_from_cart, OrderSummary,
+                    remove_single_item_from_cart, payment_handler)
 
 app_name = 'main'
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('index/', index, name="index"),
-    path('checkout/', checkout, name="checkout"),
+    path('checkout/', CheckoutView.as_view(), name="checkout"),
     path('product/<slug>', DetailProductView.as_view(), name="product-detail"),
     path('add-to-cart/<slug>', add_to_cart, name="add-to-cart"),
     path('order-summary/', OrderSummary.as_view(), name="order-summary"),
     path('remove-from-cart/<slug>', remove_from_cart, name="remove-from-cart"),
     path('remove-single-item-from-cart/<slug>', remove_single_item_from_cart, name="remove-single-item-from-cart"),
+    path('paytm/', payment_handler, name="paytm_handler")
 ]
